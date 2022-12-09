dc4edd911b7cd3b16f793cf4aa35a892b29e136e
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-09 07:47:43 -0300

    Uploading files

---
+++ modulo_02/02_IO.rb
@@ -0,0 +1,14 @@
+puts "Digite seu nome:";
+name = gets.chomp
+puts "O seu nome é " + name
+
+puts "======================="
+
+puts "Com inspect >> " + name.inspect
+
+puts "========================"
+
+puts "Digite seu salário:"
+wage = gets.chomp.to._f
+
+puts "Seu salário atualizado é: " + (wage * 1.10)to_S
