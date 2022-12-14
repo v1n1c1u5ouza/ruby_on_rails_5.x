8bc543c89fb4fc21dc439510333cb141b7dd5a41
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-14 10:53:32 -0300

    Feat: Healper to show what ambient you are

--- modulo_03/crypto_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypto_wallet/app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def date_to_br(date_us)
         date_us.strftime("%d/%m/%y")
     end
+
+    def rails_ambient
+       if Rails.env.development?
+        "Desenvolvimento"
+       elsif Rails.env.production?
+        "Produção"
+       else 
+        "Teste"   
+       end
+    end
 end
