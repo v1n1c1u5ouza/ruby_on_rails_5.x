0d94bfa88ad37a5873f2b46e170bed71cbc54144
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-15 10:40:52 -0300

    Refac: seeds.rb

--- modulo_03/crypto_wallet/db/seeds.rb
+++ modulo_03/crypto_wallet/db/seeds.rb
@@ -5,26 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...")
+spinner.auto_spin
 
+coins = [
+            {
+                description: "Bitcoins",
+                acronym: "BTC",
+                url_image: "https://pngimg.com/uploads/bitcoin/small/bitcoin_PNG47.png"
+            },
+            {
+                description: "Ethereum",
+                acronym: "ETC",
+                url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+            },
+            {    
+                description: "Dash",
+                acronym: "DASH",
+                url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+            },
+]
 
-Coin.create!(
-    description: "Bitcoins",
-    acronym: "BTC",
-    url_image: "https://pngimg.com/uploads/bitcoin/small/bitcoin_PNG47.png"
-)
+coins.each do |coin|
+    Coin.find_or_create_by(coin)
+end
 
-
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETC",
-    url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
-)
-
-
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
-)
-
-puts "Moedas cadastradas com sucesso"
+spinner.success("Moedas cadastradas com sucesso")
