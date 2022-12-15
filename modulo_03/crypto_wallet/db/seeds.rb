3d2ef37387492dcaf95d7689e8bea90c1d676661
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-15 08:49:58 -0300

    Feat: mechanism for the case we erase all the data, we can fill new ones without much effort

--- modulo_03/crypto_wallet/db/seeds.rb
+++ modulo_03/crypto_wallet/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Coin.create!(
+    description: "Bitcoins",
+    acronym: "BTC",
+    url_image: "https://pngimg.com/uploads/bitcoin/small/bitcoin_PNG47.png"
+)
+
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETC",
+    url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+)
+
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+)
+
+puts "Moedas cadastradas com sucesso"
