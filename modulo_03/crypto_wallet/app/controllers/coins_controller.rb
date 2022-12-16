037716bf7ff86989955a8666281bed00a07275f9
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-12-16 13:35:05 -0300

    Feat: Select type of mining input

--- modulo_03/crypto_wallet/app/controllers/coins_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/coins_controller.rb
@@ -1,6 +1,7 @@
 class CoinsController < ApplicationController
   layout "adm"
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
   def index
@@ -59,13 +60,17 @@ class CoinsController < ApplicationController
   end
 
   private
+    # Use callback to share mining types options
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)    
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
     end
-
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
